e5a3d8a3f0e7ca8b1823b200a78028e4d1725b1a
neolink557 <neolink557@gmail.com>
2020-01-11 20:45:06 -0500

    created the page of the profile in the django admin

--- platzigram/users/models.py
+++ platzigram/users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.                  auth.models import User
 # Create your models here.
 
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     website = models.URLField(max_length = 200, blank = True)
