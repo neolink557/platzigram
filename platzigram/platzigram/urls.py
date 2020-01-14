ba32c633d12a7dc58280cf059e8ee1d97c799bb8
neolink557 <neolink557@gmail.com>
2020-01-13 20:46:11 -0500

    created the posts view

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from posts import views as posts_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',posts_views.list_posts)
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_posts(request):
+    return render(request, 'posts/feed.html')
