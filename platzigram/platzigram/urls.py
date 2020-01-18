adfe090dd3775223428bd58a69456faa3ad3f21b
neolink557 <neolink557@gmail.com>
2020-01-18 16:31:25 -0500

    ProfileCompletionMiddleware installed

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -24,6 +24,8 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',posts_views.list_posts,name='feed'),
-    path('users/login',users_views.login_view,name='login'),
-    path('users/logout',users_views.logout_view,name='logout'),
+    path('users/login/',users_views.login_view,name='login'),
+    path('users/logout/',users_views.logout_view,name='logout'),
+    path('users/signup/',users_views.signup_view,name='signup'),
+    path('users/me/profile',users_views.update_profile_view,name='update_profile'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from users.models import Profile
 # Create your views here.
+
+def update_profile_view(request):
+    return render(request,'users/update_profile.html' )
 
 def login_view(request):
     if request.method == 'POST':
@@ -14,6 +20,27 @@ def login_view(request):
         else:
             return render(request, 'users/login.html',{'error': 'Invalid username and password'})
     return render(request, 'users/login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+        try:
+            user = User.objects.create_user(username = username, password = passwd)
+        except IntegrityError:
+            return render(request, "users/signup.html", {'error' : 'Username is already in use'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+        profile = Profile(user = user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
 
 @login_required
 def logout_view(request):
